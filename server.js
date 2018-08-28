b64a8fda7f828dfdb3a467d6552e297c9731f5f5
Kris3579 <kristianesvelt@hotmail.com>
2018-08-28 13:30:10 -0700

    progress on feature 2

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const express = require('express');
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-//const conString = 'postgres://kris3579:meowmeow@HOST:POST/books_app';
-const conString = 'postgres://localhost:5432';
+const conString = 'postgres://kris3579:meowmeow@localhost:5432/books_app';
+// const conString = 'postgres://localhost:5432';
 const client = new pg.Client(conString);
 client.connect();
 
@@ -21,10 +21,16 @@ app.get('/books',(req, res) => {
   req.query(SQL)
     .then(data => {
       let bookData = data.rows;
-      res.render('bookData', {items});
+      res.render('index', {items});
+    })
+    .catch(error => {
+      console.error(error);
     })
 })
 
 app.use(express.static(__dirname + '/public'));
 app.listen(PORT, () => console.log('server up on ', PORT));
 
+function renderBooks(rows) {
+
+} 
