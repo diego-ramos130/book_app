ea895809e6e9087a9f75299d46fcbb175559c1ba
diego-ramos130 <diego.onethreezero@gmail.com>
2018-08-28 13:41:10 -0700

    i dont know whats going on lol

--- server.js
+++ server.js
@@ -4,23 +4,23 @@ const express = require('express');
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-const conString = 'postgres://kris3579:meowmeow@localhost:5432/books_app';
-// const conString = 'postgres://localhost:5432';
+//const conString = 'postgres://kris3579:meowmeow@localhost:5432/books_app';
+const conString = 'postgres://localhost:5432';
 const client = new pg.Client(conString);
 client.connect();
 
 app.set('view engine', 'ejs');
 
 
-app.get('/', (req, res) =>{
+/* app.get('/', (req, res) =>{
   res.render('index');
-} )
+} ) */
 
-app.get('/books',(req, res) => {
-  let SQL = 'SELECT  , author, image_url FROM books';
-  req.query(SQL)
+app.get('/',(req, res) => {
+  let SQL = 'SELECT title, author, image_url FROM books';
+  client.query(SQL)
     .then(data => {
-      let bookData = data.rows;
+      let items = data.rows;
       res.render('index', {items});
     })
     .catch(error => {
