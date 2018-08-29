0dbc0c34055d87723b75ab8d95e1f3cbecae6c04
Kris3579 <kristianesvelt@hotmail.com>
2018-08-29 10:19:19 -0700

    tuesday work complete

--- server.js
+++ server.js
@@ -4,9 +4,10 @@ const express = require('express');
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-//const conString = 'postgres://kris3579:meowmeow@localhost:5432/books_app';
+const conString = 'postgres://kris3579:meowmeow@localhost:5432/books_app';
 //const conString = 'postgres://localhost:5432/books_app';
-const client = new pg.Client(process.env.DATABASE_URL);
+const client = new pg.Client(conString);
+// const client = new pg.Client(process.env.DATABASE_URL);
 client.connect();
 app.set('view engine', 'ejs');
 
@@ -39,9 +40,6 @@ function throwError (response, err) {
 
 app.use(express.static(__dirname + '/public'));
 
-// app.use('*', (req, res) => res.render('pages/error'));
-
-app.listen(PORT, () => console.log('server up on ', PORT));
-
+app.use('*', (req, res) => res.render('pages/error'));
 
 app.listen(PORT, () => console.log('server up on ', PORT));
