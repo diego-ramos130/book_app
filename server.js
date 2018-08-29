d32739835bb277ee951a6a767fbb26ce623b9659
Kris3579 <kristianesvelt@hotmail.com>
2018-08-29 14:44:09 -0700

    more css

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 //const conString = 'postgres://kris3579:meowmeow@localhost:5432/books_app';
 // const conString = 'postgres://localhost:5432/books_app';
 //const client = new pg.Client(conString);
- const client = new pg.Client(process.env.DATABASE_URL);
+const client = new pg.Client(process.env.DATABASE_URL);
 client.connect();
 
 app.set('view engine', 'ejs');
